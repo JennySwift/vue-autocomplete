697bfea718465d3e076dae5e45ea040092d0f6af
Jenny Swift <cheezyspaghetti@gmail.com>
2016-09-14 22:54:48 +1000

    Add tests

--- test/AutocompleteTest.js
+++ test/AutocompleteTest.js
@@ -10,19 +10,59 @@ describe('autocomplete component', function () {
     //     vm = new Vue(require('../AutocompleteComponent.vue'));
     // });
 
-    it('can show the dropdown', function () {
-        assert.isFalse(vm.dropdown);
-        vm.showDropdown();
-        assert.isTrue(vm.dropdown);
+    vm.unfilteredAutocompleteOptions = [
+        {name: 'one'},
+        {name: 'two'},
+        {name: 'three'}
+    ];
+
+    describe('dropdown visibility', function () {
+        it('can show the dropdown', function () {
+            assert.isFalse(vm.dropdown);
+            vm.showDropdown();
+            assert.isTrue(vm.dropdown);
+        });
+
+        it('can hide the dropdown', function () {
+            assert.isTrue(vm.dropdown);
+            vm.hideDropdown();
+            assert.isFalse(vm.dropdown);
+        });
+    });
+    
+    describe('currentIndex', function () {
+        vm.autocompleteOptions = [
+            {name: 'one'},
+            {name: 'two'},
+            {name: 'three'}
+        ];
+
+        it('can increase the currentIndex', function () {
+            assert.equal(0, vm.currentIndex);
+            vm.downArrow();
+            assert.equal(1, vm.currentIndex);
+        });
+
+        it('cannot increase the currentIndex if the currentIndex is the last item', function () {
+            vm.currentIndex = 2;
+            assert.equal(2, vm.currentIndex);
+            vm.downArrow();
+            assert.equal(2, vm.currentIndex);
+        });
+
+        it('can decrease the currentIndex', function () {
+            assert.equal(2, vm.currentIndex);
+            vm.upArrow();
+            assert.equal(1, vm.currentIndex);
+        });
+
+        it('cannot increase the currentIndex if the currentIndex is the last item', function () {
+            vm.currentIndex = 0;
+            assert.equal(0, vm.currentIndex);
+            vm.upArrow();
+            assert.equal(0, vm.currentIndex);
+        });
     });
 
-    it('can hide the dropdown', function () {
-        assert.isTrue(vm.dropdown);
-        vm.hideDropdown();
-        assert.isFalse(vm.dropdown);
-    });
 
-    it('can increase the currentIndex', function () {
-        assert.equal(0, vm.currentIndex);
-    })
 });
